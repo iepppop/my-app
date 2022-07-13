3530e32982b2fa7a3b4415a3fad8bf773d2a14b2
iepppop <iepppop@gmail.com>
2022-07-14 03:08:10 +0900

    m

--- src/components/hooks/useAnimation.js
+++ src/components/hooks/useAnimation.js
@@ -1,7 +1,20 @@
-export const Animation = () => {
+export const useAnimation = () => {
+   
     const transition= {
-        duration:2,
+        duration: 1,
         ease:[0.6,-0.05,0.01, 0.9],
     };
-    return{ transition } ;
-}
+
+    const textReveal={
+        initial: {
+            y:"200%" ,
+            opacity:0,
+        },
+        animate:{
+            y:"0%",
+            opacity:1,
+        },
+    };
+
+    return { transition, textReveal };
+};
